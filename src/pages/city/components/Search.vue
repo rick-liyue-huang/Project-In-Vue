3ed8d856feb1d8541caa7d1a1077297978a28fb6
rick.liyue.huang <rick.liyue.huang@gmail.com>
2020-04-27 11:59:47 +0800

    complete search part

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,13 +1,80 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="please input..." />
+  <div>
+    <div class="search">
+      <input
+        class="search-input"
+        type="text"
+        placeholder="please input..."
+        v-model="keyword"
+      />
+    </div>
+    <!-- 通过keyword来显示或者隐藏搜索列表 -->
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li
+          class="search-item border-bottom"
+          v-for="item of list"
+          :key="item.id"
+        >
+          {{ item.name }}
+        </li>
+        <li class="search-item border-bottom" v-show="hasNoData">
+          No Result...
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 /* eslint-disable */
+import Bscroll from "better-scroll";
 export default {
-  name: "CitySearch"
+  name: "CitySearch",
+  data: function() {
+    return {
+      keyword: "",
+      list: [],
+      timer: null
+    };
+  },
+  props: {
+    cities: Object
+  },
+  computed: {
+    hasNoData: function() {
+      return !this.list.length;
+    }
+  },
+  watch: {
+    keyword: function() {
+      if (this.timer) {
+        clearTimeout(this.timer);
+      }
+      // 让列表实时反映输入内容
+      if (!this.keyword) {
+        this.list = [];
+        return;
+      }
+      this.timer = setTimeout(() => {
+        const result = [];
+        for (let i in this.cities) {
+          this.cities[i].forEach(value => {
+            if (
+              value.spell.indexOf(this.keyword) > -1 ||
+              value.name.indexOf(this.keyword) > -1
+            ) {
+              result.push(value);
+            }
+          });
+        }
+        this.list = result;
+      }, 100);
+    }
+  },
+  mounted: function() {
+    this.scroll = new Bscroll(this.$refs["search"]); // 为了滚动
+  }
 };
 </script>
 
@@ -30,4 +97,20 @@ export default {
     padding: 0 0.1rem;
   }
 }
+.search-content {
+    z-index: 1;
+    overflow: hidden;
+    position: absolute;
+    top: 1.58rem
+    left: 0
+    right: 0;
+    bottom: 0;
+    background: #eee;
+    .search-item {
+      line-height: .62rem;
+      padding-left: .2rem;
+      color: #666;
+      background: #fff;
+    }
+  }
 </style>

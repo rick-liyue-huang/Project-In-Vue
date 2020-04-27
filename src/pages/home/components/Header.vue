f3269c8b2c4bc2d5b015a33c3e226af5122adee0
rick.liyue.huang <rick.liyue.huang@gmail.com>
2020-04-27 08:05:56 +0800

    city header

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,13 @@
       <span class="iconfont">&#xe632;</span>
       please input...
     </div>
-    <div class="header-right">
-      {{ this.city }}
-      <span class="iconfont arrow-icon">&#xe6aa;</span>
-    </div>
+    <!-- 路由跳转就使用 router-link -->
+    <router-link to="/city">
+      <div class="header-right">
+        {{ this.city }}
+        <span class="iconfont arrow-icon">&#xe6aa;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -24,38 +27,54 @@ export default {
 };
 </script>
 
-// 用 stylus 写样式， 并且需要 只是对当前组件有效 scoped // 1rem = html
-font-size = 50px, 因此 43px = 0.86rem
+
 <style lang="stylus" scoped>
 // css文件样式引用需要用 @import, 并且 引用路径 ~@ 因为有些样式需要全局使用，
-@import '~@/assets/styles/variable.styl'
+@import '~@/assets/styles/variable.styl';
+
 // @import '~styles/variable.styl'; // 需要处理webpack配置文件
-.header
-  display: flex
-  line-height: 0.86rem
-  background: $bgColor
-  color: #fff
-  .header-left
-    width: .9rem
-    float: left
-    .back-icon
-      text-align: center
-      font-size: .4rem
-  .header-input
-    flex: 1
-    background: #fff
-    border-radius: .1rem
-    height: .64rem
-    line-height: .64rem
-    margin-top: .12rem
-    margin-left: .2rem
-    padding-left: .2rem
-    color: #ccc
-  .header-right
-    width: 1.24rem
-    float right
-    text-align: center
-    .arrow-icon
-      font-size: .24rem
-      margin-left: -.04rem
+.header {
+  display: flex;
+  line-height: $headerHeight;
+  background: $bgColor;
+  color: #fff;
+
+  .header-left {
+    width: 0.9rem;
+    float: left;
+
+    .back-icon {
+      text-align: center;
+      font-size: 0.4rem;
+    }
+  }
+
+  .header-input {
+    flex: 1;
+    background: #fff;
+    border-radius: 0.1rem;
+    height: 0.64rem;
+    line-height: 0.64rem;
+    margin-top: 0.12rem;
+    margin-left: 0.2rem;
+    padding-left: 0.2rem;
+    color: #ccc;
+  }
+
+  .header-right {
+    width: 1.24rem;
+    float: right;
+    text-align: center;
+    color: #fff;
+
+    .arrow-icon {
+      font-size: 0.24rem;
+      margin-left: -0.04rem;
+    }
+  }
+}
 </style>
+
+
+// 用 stylus 写样式， 并且需要 只是对当前组件有效 scoped // 1rem = html
+font-size = 50px, 因此 43px = 0.86rem
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Vue from "vue";
 import Router from "vue-router";
 // @表示 src目录
 import Home from "@/pages/home/Home";
+import City from "@/pages/city/City";
 
 Vue.use(Router);
 
@@ -12,6 +13,11 @@ export default new Router({
       path: "/",
       name: "Home",
       component: Home
+    },
+    {
+      path: "/city",
+      name: "City",
+      component: City
     }
   ]
 });

d776993b322a740ccaaa20c136c2b4648ed72f84
rick.liyue.huang <rick.liyue.huang@gmail.com>
2020-05-29 17:48:27 +0800

    add recommend

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -92,39 +92,41 @@ export default {
 };
 </script>
 
-<style lang="stylus">
+<style lang="stylus" scoped>
 @import '~@/assets/styles/variable.styl'
 @import '~@/assets/styles/mixins.styl'
 .icons >>> .swiper-container
   height: 0
   padding-bottom: 50%
-.icon
-  width: 25%
-  height: 0
-  float: left
-  padding-bottom: 25%
-  overflow: hidden
-  position: relative
-  .icon-image
-    position:absolute
-    top: 0
-    left: 0
-    right: 0
-    bottom: .44rem
-    box-sizing: border-box
-    padding: .1rem
-    .icon-img-content
-      height: 100%
-      display: block
-      margin: 0 auto
-  .icon-desc
-    position: absolute
-    left: 0
-    right: 0
-    bottom: 0
-    height: .44rem
-    line-height: .44rem
-    color: $darkTextColor
-    text-align: center
-    ellipsis()
+.icons
+  margin-top: .2rem
+  .icon
+    width: 25%
+    height: 0
+    float: left
+    padding-bottom: 25%
+    overflow: hidden
+    position: relative
+    .icon-image
+      position:absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: .44rem
+      box-sizing: border-box
+      padding: .1rem
+      .icon-img-content
+        height: 100%
+        display: block
+        margin: 0 auto
+    .icon-desc
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      height: .44rem
+      line-height: .44rem
+      color: $darkTextColor
+      text-align: center
+      ellipsis()
 </style>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <div class="recommend-title">Recommend</div>
+    <ul>
+      <li
+        class="recommend-item border-bottom"
+        v-for="item of recommendList"
+        :key="item.id"
+      >
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" alt="" />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{ item.title }}</p>
+          <p class="item-desc">{{ item.desc }}</p>
+          <button class="item-button">Detail</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeRecommend",
+  data() {
+    return {
+      recommendList: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1505/f5/f5f45e1a83537bcb.water.jpg_200x200_444cc7e8.jpg",
+          title: "Title",
+          desc: "Description"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1505/f5/f5f45e1a83537bcb.water.jpg_200x200_444cc7e8.jpg",
+          title: "Title",
+          desc: "Description"
+        },
+        {
+          id: "0003",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/1505/f5/f5f45e1a83537bcb.water.jpg_200x200_444cc7e8.jpg",
+          title: "Title",
+          desc: "Description"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+/* min-width: 0 */
+@import '~@/assets/styles/mixins.styl'
+.recommend-title
+  line-height: .8rem
+  background: #eee
+  text-indent: .2rem
+  margin-top: .2rem
+.recommend-item
+  display: flex
+  height: 1.9rem
+  overflow: hidden
+  .item-img
+    width: 1.7rem
+    height: 1.7rem
+    padding: .1rem
+  .item-info
+    flex: 1
+    padding: .1rem
+    min-width: 0
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      font-size: .28rem
+      color: #ccc
+      ellipsis()
+    .item-button
+      background: #ff9300
+      padding: 0 .1rem
+      border-radius: .06rem
+      margin-top: .2rem
+      line-height: .44rem
+      margin-top: .16rem
+      color: #fff
+</style>

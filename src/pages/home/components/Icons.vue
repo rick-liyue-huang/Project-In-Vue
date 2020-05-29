ad0b5eebc1123b51b57c5b333992e0960877ddee
rick.liyue.huang <rick.liyue.huang@gmail.com>
2020-05-29 17:16:10 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-image">
+            <img class="icon-img-content" :src="item.imgUrl" alt="" />
+          </div>
+          <p class="icon-desc">{{ item.desc }}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeIcons",
+  data() {
+    return {
+      iconList: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          desc: "citycitycitycitycosdfdshdsj"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png",
+          desc: "city"
+        },
+        {
+          id: "0003",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/4c/eac47dd8def8de02.png",
+          desc: "city"
+        },
+        {
+          id: "0004",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png",
+          desc: "city"
+        },
+        {
+          id: "0005",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png",
+          desc: "city"
+        },
+        {
+          id: "0006",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/67/9a1678221b8e0e02.png",
+          desc: "city"
+        },
+        {
+          id: "0007",
+          imgUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/a40ee278d67000f2a29d2e20f6a029b3.png",
+          desc: "city"
+        },
+        {
+          id: "0008",
+          imgUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/a40ee278d67000f2a29d2e20f6a029b3.png",
+          desc: "city"
+        },
+        {
+          id: "0009",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/bd/9f7b9b2b60c1502.png",
+          desc: "city"
+        }
+      ]
+    };
+  },
+  computed: {
+    pages() {
+      const pages = [];
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8);
+        if (!pages[page]) {
+          pages[page] = [];
+        }
+        pages[page].push(item);
+      });
+      return pages;
+    }
+  }
+};
+</script>
+
+<style lang="stylus">
+@import '~@/assets/styles/variable.styl'
+@import '~@/assets/styles/mixins.styl'
+.icons >>> .swiper-container
+  height: 0
+  padding-bottom: 50%
+.icon
+  width: 25%
+  height: 0
+  float: left
+  padding-bottom: 25%
+  overflow: hidden
+  position: relative
+  .icon-image
+    position:absolute
+    top: 0
+    left: 0
+    right: 0
+    bottom: .44rem
+    box-sizing: border-box
+    padding: .1rem
+    .icon-img-content
+      height: 100%
+      display: block
+      margin: 0 auto
+  .icon-desc
+    position: absolute
+    left: 0
+    right: 0
+    bottom: 0
+    height: .44rem
+    line-height: .44rem
+    color: $darkTextColor
+    text-align: center
+    ellipsis()
+</style>

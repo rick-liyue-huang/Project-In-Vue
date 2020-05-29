fc55309c9b39a0e77d12dee089ad6d9eac928c0b
rick.liyue.huang <rick.liyue.huang@gmail.com>
2020-05-29 18:05:06 +0800

    add weekend

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="weekend-title">Weekend Play</div>
+    <ul>
+      <li
+        class="weekend-item border-bottom"
+        v-for="item of recommendList"
+        :key="item.id"
+      >
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" alt="" />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{ item.title }}</p>
+          <p class="item-desc">{{ item.desc }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeWeekend",
+  data() {
+    return {
+      recommendList: [
+        {
+          id: "0001",
+          imgUrl:
+            "https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20204/17c0f864dd33bb2c2b2be2299c6da59d.jpeg",
+          title: "Title",
+          desc: "Description"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20205/893a01295971bcf57c6ebd52bcebb91d.jpg",
+          title: "Title",
+          desc: "Description"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+/* min-width: 0 */
+@import '~@/assets/styles/mixins.styl'
+.weekend-title
+  line-height: .8rem
+  background: #eee
+  text-indent: .2rem
+  margin-top: .2rem
+.weekend-img-wrapper
+  height: 0
+  overflow: hidden
+  padding-bottom: 33.9%
+.item-img
+  width: 100%
+.item-info
+  padding: .1rem
+  .item-title
+    line-height: .54rem
+    font-size: .32rem
+    ellipsis()
+  .item-desc
+    line-height: .4rem
+    font-size: .28rem
+    color: #ccc
+    ellipsis()
+</style>

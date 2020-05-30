4667363ef6dc29d7700effb2bc0407a43e5e3ed6
rick.liyue.huang <rick.liyue.huang@gmail.com>
2020-05-30 08:19:58 +0800

    mounted axios

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -13,58 +13,19 @@
       <div class="area">
         <div class="title border-topbottom">Hot city</div>
         <div class="button-list">
-          <div class="button-wrapper">
-            <div class="button">Melbourne</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">Melbourne</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">Melbourne</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">Melbourne</div>
+          <div class="button-wrapper" v-for="item of hotCities" :key="item.id">
+            <div class="button">{{ item.name }}</div>
           </div>
         </div>
       </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
+      <div class="area" v-for="(item, key) of cities" :key="key">
+        <div class="title border-topbottom">{{ key }}</div>
         <div class="item-list">
-          <div class="item border-bottom">alaer</div>
-          <div class="item border-bottom">alaer</div>
-          <div class="item border-bottom">alaer</div>
-          <div class="item border-bottom">alaer</div>
-          <div class="item border-bottom">alaer</div>
-        </div>
-      </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item border-bottom">alaer</div>
-          <div class="item border-bottom">alaer</div>
-          <div class="item border-bottom">alaer</div>
-          <div class="item border-bottom">alaer</div>
-          <div class="item border-bottom">alaer</div>
-        </div>
-      </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item border-bottom">alaer</div>
-          <div class="item border-bottom">alaer</div>
-          <div class="item border-bottom">alaer</div>
-          <div class="item border-bottom">alaer</div>
-          <div class="item border-bottom">alaer</div>
-        </div>
-      </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item border-bottom">alaer</div>
-          <div class="item border-bottom">alaer</div>
-          <div class="item border-bottom">alaer</div>
-          <div class="item border-bottom">alaer</div>
-          <div class="item border-bottom">alaer</div>
+          <div
+            class="item border-bottom"
+            v-for="innerItem of item"
+            :key="innerItem.id"
+          >{{ innerItem.name }}</div>
         </div>
       </div>
     </div>
@@ -76,6 +37,10 @@
 import BScroll from "better-scroll";
 export default {
   name: "CityList",
+  props: {
+    hotCities: Array,
+    cities: Object
+  },
   mounted() {
     this.scroll = new BScroll(this.$refs.wrapper);
   }
